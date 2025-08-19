920f023c5b12caaad171f3838c0bb5f92815d416
Raju Singh <rs3324445@gmail.com>
2025-08-19 20:56:06 +0530

    Add files via upload

---
+++ SQL Project11.sql
@@ -0,0 +1,24 @@
+create database pizzahut;
+
+CREATE TABLE orders (
+    order_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    order_time TIME NOT NULL,
+    PRIMARY KEY (order_id)
+);
+SELECT 
+    *
+FROM
+    orders;
+
+CREATE TABLE order_details (
+    order_details_id INT NOT NULL,
+    order_id INT NOT NULL,
+    pizza_id TEXT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (order_details_id));
+SELECT *FROM
+    order_details;
+
+
+
